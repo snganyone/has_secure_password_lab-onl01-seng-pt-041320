797594b09c029cafeeef4497259df45e6554041b
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-23 15:57:32 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,13 @@ class UsersController < ApplicationController
     end
 
     def create
-        User.create(user_params)
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to login_path(@user)
+        else
+            render :new
+        end
     end
 
     def user_params
