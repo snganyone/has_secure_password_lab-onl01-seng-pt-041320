b11087eb1b37b7c4af30bdbef3221acd529e6517
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-23 17:25:20 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,12 @@
 class SessionsController < ApplicationController
-    def new
-        @user = User.find_by(id: session[:user_id])
-    end
+
     def create
-        #binding.pry
         @user = User.find_by(name: params[:user][:name])
-        if @user #&& @user.authenticate(params[:password])
+        if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
             redirect_to root_path
         else
-            render :'users/new'
+            render :'users/login'
         end
     end
 end
